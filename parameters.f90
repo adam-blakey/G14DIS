b878597b7472ae765353b44e698f9b23b81935e2
JustAdamHere <adam@blakey.family>
2019-10-15 14:49:42 +0100

    Removed the run-ability of parameters.f90.

--- parameters.f90
+++ parameters.f90
@@ -4,10 +4,3 @@ module parameters
     integer, parameter :: dp = selected_real_kind(15)
     real(dp), parameter :: PI = 3.1415926536
 end module parameters
-
-program test
-    use parameters
-    implicit none
-
-    print *, PI
-end program test

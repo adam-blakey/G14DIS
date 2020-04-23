1494899201225b8dea79b27bf317285dd552a1ee
JustAdamHere <adam@blakey.family>
2020-04-23 12:52:31 +0100

    + Added nonlinear declarations and definitions.

---
+++ src/nonlinearSystems.cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+ * NONLINEARSYSTEMS.CPP
+ *
+ * This is a file containing functions regarding nonlinear systems.
+ * 
+ * @author     Adam Matthew Blakey
+ * @date       2020/04/23
+ ******************************************************************************/
+
+#include "nonlinearSystems.hpp"
+#include <cassert>
+#include <vector>
+
+#include <iostream>
+
+/******************************************************************************
+ * thomasInvert
+ * 
+ * @details    Inverts a tridiagonal matrix.
+ *
+ * @param[in] n 			Gives the degree of the polynomial.
+ * @param[in] x 			The point at which to evaluate the polynomial.
+ ******************************************************************************/
+void nonlinearSystems::Newton(f, f_, u0, )
+{
+	
+}
